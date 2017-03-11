857e6bf543963225e50585a7be3cf00754e26da9
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-03-11 18:50:01 -0400

    add datepickers and select2 in forms

--- web/js/main.js
+++ web/js/main.js
@@ -4,10 +4,21 @@ $(document).ready(function() {
 	$("#role-permisos").select2();
 	$("#course-category_id").select2();
 	$("#schedule-course_id").select2();
+	$("#instructor-user_id").select2();
+	$("#instructor-schedule_id").select2();
+	$("#instructor-schedule_id").select2();
+	$("#registered-user_id").select2();
+	$("#registered-schedule_id").select2();
+	$("#preregistered-user_id").select2();
+	$("#preregistered-schedule_id").select2();
+	$("#payment-preregister_id").select2();
+	$("#payment-payment_type").select2();
+	$("#interestlist-user_id").select2();
+	$("#interestlist-course_id").select2();
+	$("#postulate-schedule_id").select2();
+	$("#postulate-user_id").select2();
 	//$("#").select2();
-	//$("#").select2();
-	//$("#").select2();
-	//$("#").select2();
+
 
 	// Datepicker
 	$("#schedule-start_date").datepicker({
@@ -20,6 +31,20 @@ $(document).ready(function() {
 	 	format: 'yyyy-mm-dd',
 	 	startDate: '0d'
 	});
+
+	$("#preregistered-preregister_date").datepicker({
+	 	language: 'es',
+	 	format: 'yyyy-mm-dd',
+	 	startDate: '0d'
+	});
+
+	$("#payment-payment_date").datepicker({
+	 	language: 'es',
+	 	format: 'yyyy-mm-dd',
+	 	startDate: '0d'
+	});
+
+	//Timepickers
 
 
 	$("#schedule-start_hour").timepicker({
