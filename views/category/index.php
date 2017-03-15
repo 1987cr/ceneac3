82448cb9d730ae9dcc49be654c2f036ed04cc785
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-03-14 21:27:18 -0400

    add multiple delete in categories view

--- views/category/index.php
+++ views/category/index.php
@@ -46,6 +46,7 @@ $actionColumnTemplateString = '<div class="action-buttons">'.$actionColumnTempla
     <div class="clearfix crud-navigation">
         <div class="pull-left">
             <?php echo Html::a('<span class="glyphicon glyphicon-plus"></span> ' . 'New', ['create'], ['class' => 'btn btn-success']) ?>
+						<input type="button" class="btn btn-danger" value="Borrar" id="delete-btn" >
         </div>
 
         <div class="pull-right">
@@ -89,10 +90,12 @@ $actionColumnTemplateString = '<div class="action-buttons">'.$actionColumnTempla
 			'firstPageLabel' => 'First',
 			'lastPageLabel' => 'Last',
 		],
+		'options' => ['id' => 'category-pjax'],
 		'filterModel' => $searchModel,
 		'tableOptions' => ['class' => 'table table-striped table-bordered table-hover'],
 		'headerRowOptions' => ['class'=>'x'],
 		'columns' => [
+			['class' => 'yii\grid\CheckboxColumn'],
 			[
 				'class' => 'yii\grid\ActionColumn',
 				'template' => $actionColumnTemplateString,
@@ -123,6 +126,31 @@ $actionColumnTemplateString = '<div class="action-buttons">'.$actionColumnTempla
     </div>
 
 </div>
+<?php
 
+    $this->registerJs('
+
+    $(document).ready(function(){
+
+	    $(\'#delete-btn\').click(function(){
+					var idCategories = $(\'#category-pjax\').yiiGridView(\'getSelectedRows\');
+					$.ajax({
+							type: \'POST\',
+							url : \'/web/category/multiple-delete\',
+							data : {row_id: idCategories},
+							success : function(res) {
+								if(res) {
+									alert("No se puede eliminar, un curso depende de esta categoria");
+								} else {
+									$.pjax.reload({container:\'#category-pjax\'});
+								}
+							}
+					});
+	    });
+
+
+    });', \yii\web\View::POS_READY);
+
+?>
 
 <?php \yii\widgets\Pjax::end() ?>
