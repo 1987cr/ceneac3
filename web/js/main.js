cf27d500381cad7465a204b7a29e7625bb8a0913
Carlos Riera <1987cr@gmail.com>
2017-06-18 22:37:02 -0400

    Js Refactor

--- web/js/main.js
+++ web/js/main.js
@@ -1,12 +1,114 @@
 // Debug Mode
 var debug = true;
-var start = moment().startOf("month");
-var end = moment();
+
+/* ------------------------------------- */
+/* ............... Helper ............... */
+/* ------------------------------------- */
+
+// Simula que la tecla enter ha sido presionada,
+// hace submit al formulario.
+var enterPressed = function(context) {
+  var e = jQuery.Event("keydown");
+  e.which = 13;
+  e.keyCode = 13;
+  $(context).trigger(e);
+};
+
+/* ------------------------------------- */
+/* ................ Ajax ............... */
+/* ------------------------------------- */
+
+var ajaxPost = function(model, action, successCb, errorCb) {
+  errorCb = typeof errorCb !== "undefined" ? errorCb : genericError;
+
+  var selectedRowsId = $("#pjax").yiiGridView("getSelectedRows");
+
+  if (!!selectedRowsId.length) {
+    $.ajax({
+      type: "POST",
+      url: "/web/" + model + "/" + action,
+      data: { row_id: selectedRowsId },
+      success: successCb,
+      error: errorCb
+    });
+  } else {
+    toastr["warning"]("No se seleccionaron elementos.");
+  }
+};
+
+var successMultipleDelete = function(res) {
+  !debug && $.pjax.reload({ container: "#pjax" });
+
+  var resArr = res.split(",");
+
+  var errorsNo = resArr.filter(function(el) {
+    return el === "error";
+  }).length;
+  var successNo = resArr.filter(function(el) {
+    return el === "deleted";
+  }).length;
+
+  var successMsg = successNo > 1
+    ? " elementos eliminados."
+    : " elemento eliminado.";
+  var errorMsg = (errorsNo > 1 ? " errores" : " error") + " de dependencia.";
+
+  if (successNo > 0) toastr["success"](successNo + successMsg);
+  if (errorsNo > 0) toastr["error"](errorsNo + errorMsg);
+};
+
+var genericError = function(e) {
+  console.error(e.responseText);
+  toastr["error"]("Error Interno del Servidor.");
+};
+
+/* ------------------------------------- */
+/* .............. Select2 ............... */
+/* ------------------------------------- */
+
+var CREATE_SELECTS = [
+  "#role-permisos",
+  "#course-category_id",
+  "#schedule-course_id",
+  "#instructor-user_id",
+  "#instructor-schedule_id",
+  "#instructor-schedule_id",
+  "#registered-user_id",
+  "#registered-schedule_id",
+  "#preregistered-user_id",
+  "#preregistered-schedule_id",
+  "#payment-preregister_id",
+  "#payment-payment_type",
+  "#interestlist-user_id",
+  "#interestlist-course_id",
+  "#postulate-schedule_id",
+  "#postulate-user_id"
+];
+
+var SEARCH_SELECTS = ["#schedulesearch-course_id"];
+
+var initSelect2 = function(selectors) {
+  selectors.forEach(function(sel) {
+    $(sel).select2();
+  });
+};
+
+/* ------------------------------------- */
+/* ............. Date Range ............. */
+/* ------------------------------------- */
+
+var SEARCH_DATE_RANGE = [
+  "input[name='ScheduleSearch[start_date]']",
+  "input[name='ScheduleSearch[end_date]']"
+  // "input[name='PreregisteredSearch[preregister_date]']",
+  // "input[name='InterestListSearch[start_date]']",
+  // "input[name='PaymentSearch[payment_date]']",
+];
 
 var DATE_RANGE_CONFIG = {
   autoUpdateInput: false,
-  startDate: start,
-  endDate: end,
+  startDate: moment().startOf("month"),
+  endDate: moment(),
   showDropdowns: true,
   alwaysShowCalendars: true,
   opens: "center",
@@ -43,15 +145,7 @@ var DATE_RANGE_CONFIG = {
   }
 };
 
-// Simula que la tecla enter ha sido presionada,
-// le hace submit al formulario.
-var enterPressed = function(context) {
-  var e = jQuery.Event("keydown");
-  e.which = 13;
-  e.keyCode = 13;
-  $(context).trigger(e);
-};
-
+// On Apply Callback
 var onApplyDatepicker = function(ev, picker) {
   $(this).val(
     picker.startDate.format("DD-MM-YYYY") +
@@ -61,77 +155,100 @@ var onApplyDatepicker = function(ev, picker) {
   enterPressed(this);
 };
 
+// On Cancel Callback
 var onCancelDatepicker = function(ev, picker) {
   $(this).val("");
   enterPressed(this);
 };
 
+var initDateRangePicker = function(selectors) {
+  selectors.forEach(function(sel) {
+    $(sel)
+      .daterangepicker(DATE_RANGE_CONFIG)
+      .on("apply.daterangepicker", onApplyDatepicker)
+      .on("cancel.daterangepicker", onCancelDatepicker);
+  });
+};
+
+/* ------------------------------------- */
+/* ............ Date Picker ............. */
+/* ------------------------------------- */
+
+var DATE_PICKER_CONFIG = {
+  language: "es",
+  format: "dd-mm-yyyy",
+  startDate: "0d"
+};
+
+var CREATE_DATE_PICKERS = [
+  "#schedule-start_date",
+  "#schedule-end_date",
+  "#preregistered-preregister_date",
+  "#payment-payment_date",
+  "#interestlist-start_date"
+];
+
+var initDatePicker = function(selectors) {
+  selectors.forEach(function(sel) {
+    $(sel).datepicker(DATE_PICKER_CONFIG);
+  });
+};
+
+/* ------------------------------------- */
+/* ............ Time Picker ............. */
+/* ------------------------------------- */
+
+var TIME_PICKER_CONFIG = {
+  timeFormat: "h:mm p",
+  interval: 30,
+  minTime: "10:00am",
+  maxTime: "6:00pm",
+  startTime: "10:00",
+  dropdown: true,
+  scrollbar: true
+};
+
+var CREATE_TIME_PICKERS = ["#schedule-start_hour", "#schedule-end_hour"];
+
+var SEARCH_TIME_PICKERS = [
+  "input[name='ScheduleSearch[start_hour]']",
+  "input[name='ScheduleSearch[end_hour]']"
+];
+
+var initTimePicker = function(selectors) {
+  selectors.forEach(function(sel) {
+    $(sel).timepicker(TIME_PICKER_CONFIG);
+  });
+};
+
+/* ------------------------------------- */
+/* ................ Pjax ................ */
+/* ------------------------------------- */
+
+// Llamado después de actualizar un search
+$(document).on("pjax:success", function() {
+  initSelect2(SEARCH_SELECTS);
+  initDateRangePicker(SEARCH_DATE_RANGE);
+  initTimePicker(SEARCH_TIME_PICKERS);
+});
+
+/* ------------------------------------- */
+/* ........... Document Ready ............ */
+/* ------------------------------------- */
+
 $(document).ready(function() {
-  /* ------------------------------------- */
-  /* .............. Select2 .............. */
-  /* ------------------------------------- */
+  initSelect2(CREATE_SELECTS);
+  initSelect2(SEARCH_SELECTS);
 
-  $("#role-permisos").select2();
-  $("#course-category_id").select2();
-  $("#schedule-course_id").select2();
-  $("#instructor-user_id").select2();
-  $("#instructor-schedule_id").select2();
-  $("#instructor-schedule_id").select2();
-  $("#registered-user_id").select2();
-  $("#registered-schedule_id").select2();
-  $("#preregistered-user_id").select2();
-  $("#preregistered-schedule_id").select2();
-  $("#payment-preregister_id").select2();
-  $("#payment-payment_type").select2();
-  $("#interestlist-user_id").select2();
-  $("#interestlist-course_id").select2();
-  $("#postulate-schedule_id").select2();
-  $("#postulate-user_id").select2();
+  initDateRangePicker(SEARCH_DATE_RANGE);
 
-  /* ------------------------------------- */
-  /* ............ Date Picker ............ */
-  /* ------------------------------------- */
+  initDatePicker(CREATE_DATE_PICKERS);
 
-  $("#schedule-start_date").datepicker({
-    language: "es",
-    format: "dd-mm-yyyy",
-    startDate: "0d"
-  });
+  initTimePicker(CREATE_TIME_PICKERS);
+  initTimePicker(SEARCH_TIME_PICKERS);
 
-  $("#schedule-end_date").datepicker({
-    language: "es",
-    format: "dd-mm-yyyy",
-    startDate: "0d"
-  });
-
-  $("input[name='ScheduleSearch[start_date]']")
-    .daterangepicker(DATE_RANGE_CONFIG)
-    .on("apply.daterangepicker", onApplyDatepicker)
-    .on("cancel.daterangepicker", onCancelDatepicker);
-
-  $("input[name='ScheduleSearch[end_date]']")
-    .daterangepicker(DATE_RANGE_CONFIG)
-    .on("apply.daterangepicker", onApplyDatepicker)
-    .on("cancel.daterangepicker", onCancelDatepicker);
-
-  $(document).on("pjax:success", function() {
-    $("input[name='ScheduleSearch[start_date]']")
-      .daterangepicker(DATE_RANGE_CONFIG)
-      .on("apply.daterangepicker", onApplyDatepicker)
-      .on("cancel.daterangepicker", onCancelDatepicker);
-
-    $("input[name='ScheduleSearch[end_date]']")
-      .daterangepicker(DATE_RANGE_CONFIG)
-      .on("apply.daterangepicker", onApplyDatepicker)
-      .on("cancel.daterangepicker", onCancelDatepicker);
-  });
-
-  $("input[name='PaymentSearch[payment_date]']").datepicker({
-    language: "es",
-    format: "dd-mm-yyyy",
-    startDate: new Date("2015")
-  });
-
+  // TO-DO: Cambiar estos a date range, modificar SearchClass de cada uno para dividir la fecha
+  // igual que el ScheduleSearch
   $("input[name='PreregisteredSearch[preregister_date]']").datepicker({
     language: "es",
     format: "dd-mm-yyyy",
@@ -144,103 +261,15 @@ $(document).ready(function() {
     startDate: new Date("2015")
   });
 
-  $("#preregistered-preregister_date").datepicker({
+  $("input[name='PaymentSearch[payment_date]']").datepicker({
     language: "es",
     format: "dd-mm-yyyy",
-    startDate: "0d"
+    startDate: new Date("2015")
   });
-
-  $("#payment-payment_date").datepicker({
-    language: "es",
-    format: "dd-mm-yyyy",
-    startDate: "0d"
-  });
-
-  $("#interestlist-start_date").datepicker({
-    language: "es",
-    format: "dd-mm-yyyy",
-    startDate: "0d"
-  });
-
-  /* ------------------------------------- */
-  /* ............ Time Picker ............ */
-  /* ------------------------------------- */
-
-  $("#schedule-start_hour").timepicker({
-    timeFormat: "h:mm p",
-    interval: 30,
-    minTime: "10:00am",
-    maxTime: "6:00pm",
-    defaultTime: "10:00am",
-    startTime: "10:00",
-    dynamic: true,
-    dropdown: true,
-    scrollbar: true
-  });
-
-  $("#schedule-end_hour").timepicker({
-    timeFormat: "h:mm p",
-    interval: 30,
-    minTime: "10:00am",
-    maxTime: "6:00pm",
-    defaultTime: "10:00am",
-    startTime: "10:00",
-    dynamic: true,
-    dropdown: true,
-    scrollbar: true
-  });
-
-  /* ------------------------------------- */
-  /* ................ Ajax ............... */
-  /* ------------------------------------- */
-
-  function ajaxPost(model, action, successCb, errorCb) {
-    errorCb = typeof errorCb !== "undefined" ? errorCb : genericError;
-
-    var selectedRowsId = $("#pjax").yiiGridView("getSelectedRows");
-
-    if (!!selectedRowsId.length) {
-      $.ajax({
-        type: "POST",
-        url: "/web/" + model + "/" + action,
-        data: { row_id: selectedRowsId },
-        success: successCb,
-        error: errorCb
-      });
-    } else {
-      toastr["warning"]("No se seleccionaron elementos.");
-    }
-  }
-
-  function successMultipleDelete(res) {
-    !debug && $.pjax.reload({ container: "#pjax" });
-
-    var resArr = res.split(",");
-
-    var errorsNo = resArr.filter(function(el) {
-      return el === "error";
-    }).length;
-    var successNo = resArr.filter(function(el) {
-      return el === "deleted";
-    }).length;
-
-    var successMsg = successNo > 1
-      ? " elementos eliminados."
-      : " elemento eliminado.";
-    var errorMsg = (errorsNo > 1 ? " errores" : " error") + " de dependencia.";
-
-    if (successNo > 0) toastr["success"](successNo + successMsg);
-    if (errorsNo > 0) toastr["error"](errorsNo + errorMsg);
-  }
-
-  function genericError(e) {
-    console.error(e.responseText);
-    toastr["error"]("Error Interno del Servidor.");
-  }
 
   /* ................. Backups ................ */
 
-  // Multiple Delete
+  // Create
   $("body").on("click", "#CreateBackup", function() {
     $.ajax({
       type: "POST",
